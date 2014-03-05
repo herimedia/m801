1574cbcb1d517d611a549159f7eb92831c7516f3
Niels Ganser <niels@herimedia.com>
2014-03-05 10:39:33 +0100

    Fix determine_coverage/thoughtbot_paperclip.sh

--- determine_coverage/thoughtbot_paperclip.sh
+++ determine_coverage/thoughtbot_paperclip.sh
@@ -9,13 +9,13 @@
 
 cd repositories/thoughtbot_paperclip
 
-rvm use 2.0.thoughtbot_paperclip
+rvm use 2.0.0@thoughtbot_paperclip
 
 if ! grep "SimpleCov.start" test/helper.rb; then
   sed -i test/helper.rb -e "1irequire 'simplecov'\nSimpleCov.start\n"
 fi
 
-bundle exec rake clean test cucumber
+bundle exec rake clean test
 
 echo -e "\n==> thoughtbot/paperclip Test Coverage:"
 cat coverage/.last_run.json
